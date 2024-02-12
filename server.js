96f72354c07d20c3c3cd9309e0e0344ebb8fda8d
Saikiran Nagulapuram <20274@arthahyd.com>
2024-02-12 17:54:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app  = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+app.get('/users/register', (req, res)=>{
+    res.render("register");
+});
+
+app.get('/users/login', (req, res)=>{
+    res.render("login");
+});
+
+app.get('/users/dashboard', (req, res)=>{
+    res.render("dashboard", {user: "sai"});
+});
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
