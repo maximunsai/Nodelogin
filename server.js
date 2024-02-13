bbaeb4bd0f26df455baffbcdc5fb6949958fe007
Saikiran Nagulapuram <20274@arthahyd.com>
2024-02-13 17:49:58 +0530

    connected to da and made some changes in views to work

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app  = express();
+const { pool } = require('./db_config')
+const bcrypt = require('bcrypt')
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res)=>{
     res.render('index');
@@ -21,7 +25,30 @@ app.get('/users/dashboard', (req, res)=>{
     res.render("dashboard", {user: "sai"});
 });
 
+app.post("/users/register", async(req, res)=>{
+    let {name, email, password, password2} = req.body;
+    console.log({name, email, password, password2});
 
+    let errors =[];
+    if (!name || !email || !password || !password2){
+        errors.push({message: "Please enter all feilds"});
+    }
+
+    if (password.length <6){
+        errors.push({message: "Password must contain 6 characers"});
+    }
+
+    if (password != password2){
+        errors.push({message: "Password do not match..!"});
+    }
+
+    if (errors.length > 0){
+        res.render("register", {errors});
+    } else{
+        let hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+    }
+})
 
 
 app.listen(PORT, ()=>{
